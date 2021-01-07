388b0039b08245137ddcc0b8441793825a984826
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2021-01-07 21:03:23 +0100

    Animate chevron

--- features/goalDetails/components/DescriptionOpenedIndicator.tsx
+++ features/goalDetails/components/DescriptionOpenedIndicator.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { ChevronDownIcon, ChevronUpIcon } from '@primer/octicons-react';
 import classNames from '../goalDetails.module.scss';
+import createClassName from 'classnames';
 
 type Props = {
     detailsVisible: boolean;
 };
 
-const DescriptionOpenedIndicator: React.FC<Props> = ({ detailsVisible }) => (
-    <div className={classNames.descriptionOpenedIndicator}>
-        {detailsVisible ? <ChevronUpIcon /> : <ChevronDownIcon size="small" />}
-    </div>
-);
+const DescriptionOpenedIndicator: React.FC<Props> = ({ detailsVisible }) => {
+    const className = createClassName(classNames.descriptionOpenedIndicator, {
+        [classNames.descriptionOpenedIndicatorIsOpened]: detailsVisible,
+    });
+
+    return (
+        <div className={className}>
+            <ChevronDownIcon size="small" />
+        </div>
+    );
+};
 
 export default DescriptionOpenedIndicator;
